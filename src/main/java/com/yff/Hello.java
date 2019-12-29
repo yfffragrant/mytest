9a658b8fdbe887cfe1bcd0d2df98d0973d3559b6
yff <yfffragrant@sina.com>
2019-12-29 20:49:43 +0800

    git1

---
+++ src/main/java/com/yff/Hello.java
@@ -0,0 +1,14 @@
+package com.yff;
+
+public class Hello {
+
+    private String str="hello word";
+
+
+    public static void main(String[] args) {
+
+        int i = 2;
+        System.out.println("helloword1");
+
+    }
+}
