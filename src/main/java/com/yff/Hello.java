965921adde243b3db7ae5010030bea0312130838
yff <yfffragrant@sina.com>
2019-12-29 21:18:00 +0800

    git1

--- src/main/java/com/yff/Hello.java
+++ src/main/java/com/yff/Hello.java
@@ -2,11 +2,11 @@ package com.yff;
 
 public class Hello {
 
-    private String str="hello word";
+    private String str="hello word1";
 
 
     public static void main(String[] args) {
-        int i = 2;
+        int i = 21;
         System.out.println("helloword1");
     }
 }
